345ba4586fa954fe0f523f0d64d6d87a63da64f7
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2021-02-23 23:31:38 -0300

    inclusão de novo campo na categoria

--- database/nerdstore.database/dbo/Tables/Categories.sql
+++ database/nerdstore.database/dbo/Tables/Categories.sql
@@ -2,6 +2,7 @@
     [Id]   UNIQUEIDENTIFIER NOT NULL,
     [Name] VARCHAR (250)    NOT NULL,
     [Code] VARCHAR (500)    NOT NULL,
+    [Active] bit    NOT NULL,
     CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
