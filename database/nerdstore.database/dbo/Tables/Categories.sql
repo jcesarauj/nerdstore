7b3b921d22217b765556c65062500032497430f4
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2021-02-23 23:04:37 -0300

    inclusão projeto de banco de dados

---
+++ database/nerdstore.database/dbo/Tables/Categories.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Name] VARCHAR (250)    NOT NULL,
+    [Code] VARCHAR (500)    NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ database/nerdstore.database/dbo/Tables/Products.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [Name]            VARCHAR (250)    NOT NULL,
+    [Description]     VARCHAR (500)    NOT NULL,
+    [Active]          BIT              NOT NULL,
+    [Price]           DECIMAL (18, 2)  NOT NULL,
+    [CreateDate]      DATETIME2 (7)    NOT NULL,
+    [Image]           VARCHAR (250)    NOT NULL,
+    [QuantityInStock] INT              NOT NULL,
+    [CategoryId]      UNIQUEIDENTIFIER NOT NULL,
+    [Height]          INT              NULL,
+    [Width]           INT              NULL,
+    [Depth]           INT              NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Products_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Products_CategoryId]
+    ON [dbo].[Products]([CategoryId] ASC);
+
